0f4e4472e66b8fe206cb792732bdbb539142aec7
Hanifullah Jamalzai <hanifullahjamalzai2014@gmail.com>
2022-01-13 20:34:21 +0430

    new method renderGame()

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let firstCard = 10;
-let secondCard = 11;
+let secondCard = 4;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
@@ -9,8 +10,12 @@ let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
+	renderGame();
+}
+
+function renderGame() {
 	sumEl.textContent = "Sum: " + sum;
-	cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+	cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
 	if (sum < 21) {
 		message = "Do you want to draw a new card?";
 	} else if (sum === 21) {
@@ -24,5 +29,7 @@ function startGame() {
 }
 
 function newCard() {
-	console.log("new card pressed");
+	let card = 7;
+	sum += card;
+	startGame();
 }
