fd513ad54bff47c0e582edc5665bfac3041806d7
Hanifullah Jamalzai <hanifullahjamalzai2014@gmail.com>
2022-01-15 12:09:03 +0430

    get random number for card

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let firstCard = 10;
-let secondCard = 4;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
 let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
@@ -9,27 +9,37 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
+// Make this function return a random number between 1 and 13
+function getRandomCard() {
+	return Math.floor(Math.random() * 13) + 1; // 0.000
+}
+
 function startGame() {
 	renderGame();
 }
 
 function renderGame() {
+	cardsEl.textContent = "Cards: ";
+	for (let i = 0; i < cards.length; i++) {
+		cardsEl.textContent += cards[i] + " ";
+	}
+
 	sumEl.textContent = "Sum: " + sum;
-	cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
-	if (sum < 21) {
+	if (sum <= 20) {
 		message = "Do you want to draw a new card?";
 	} else if (sum === 21) {
-		message = "You've got BlackJack!";
+		message = "You've got Blackjack!";
 		hasBlackJack = true;
 	} else {
-		message = "You are out of the game!";
+		message = "You're out of the game!";
 		isAlive = false;
 	}
 	messageEl.textContent = message;
 }
 
 function newCard() {
-	let card = 7;
+	let card = getRandomCard();
 	sum += card;
-	startGame();
+	cards.push(card);
+	renderGame();
 }
